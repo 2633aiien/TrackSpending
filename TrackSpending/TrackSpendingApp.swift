2c6e83ac995bf9bd1cb9fc394f11dc95979aacea
allen <allen000728@gmail.com>
2023-05-05 07:49:17 +0800

    Initial Commit

---
+++ TrackSpending/TrackSpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackSpendingApp.swift
+//  TrackSpending
+//
+//  Created by allen on 2023/5/5.
+//
+
+import SwiftUI
+
+@main
+struct TrackSpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
